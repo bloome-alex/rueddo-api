b0b0d3556811691bc2f994387406121584bc3dcc
bloome-alex <alex99valdiviezo@gmail.com>
2023-02-10 12:09:00 -0300

    disabled cors

--- server/Server.js
+++ server/Server.js
@@ -14,7 +14,7 @@ class Server{
         this.http = require('http').Server(this.app)
         this.io = require('socket.io')(this.http, {
             cors: {
-                origin: false,
+                origin: ["https://rueddo-vuelder-production.up.railway.app", "https://rueddo-mobile-production.up.railway.app"],
                 methods: ["GET", "POST"],
             }
         })
