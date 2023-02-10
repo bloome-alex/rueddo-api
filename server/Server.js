f446a7b73aa05c0a753407b3cbd8f8b9c5b19530
bloome-alex <alex99valdiviezo@gmail.com>
2023-02-10 11:21:25 -0300

    multiple cors

--- server/Server.js
+++ server/Server.js
@@ -53,8 +53,11 @@ class Server{
 
     middlewares(){
         this.app.use((req, res, next) => {
-            res.header("Access-Control-Allow-Origin", "https://rueddo-vuelder-production.up.railway.app"); 
-            res.header('Access-Control-Allow-Credentials', false);
+            const allowedOrigins = ['https://rueddo-vuelder-production.up.railway.app', 'https://rueddo-mobile-production.up.railway.app'];
+            const origin = req.headers.origin;
+            if (allowedOrigins.includes(origin)) {
+                res.setHeader('Access-Control-Allow-Origin', origin);
+            }
             res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, Access-Control-Allow-Credentials');
             res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
             res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
